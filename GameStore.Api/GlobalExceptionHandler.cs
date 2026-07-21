ce09b5143d09e7e56cc7777de8f7308c0dc5eb77
maaz8181 <maaz.hbtw@gmail.com>
2026-07-21 15:42:17 +0530

    Implement global exception handling

---
+++ GameStore.Api/GlobalExceptionHandler.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace GameStore.Api;
+
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger = logger;
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        var traceId = Activity.Current?.Id ?? httpContext.TraceIdentifier;
+
+        logger.LogError(exception, "Could not process a request on machine {MachineName}. TraceId: {TraceId}", Environment.MachineName, traceId);
+
+        var (statusCode, title) = MapException(exception); 
+
+        await Results.Problem(
+            title: title,
+            statusCode: statusCode,
+            extensions: new Dictionary<string, object?>
+            {
+                {"traceId", traceId}
+            }
+            ).ExecuteAsync(httpContext);
+            
+            return true;
+    }
+
+    private static (int statusCode, string title) MapException(Exception exception)
+    {
+        return exception switch
+        {
+            ArgumentException => (StatusCodes.Status400BadRequest, exception.Message),
+            _ => (StatusCodes.Status500InternalServerError, "We made a mistake but we're working on it!")
+        };
+    }
+}
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,3 +1,4 @@
+using GameStore.Api;
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
@@ -6,7 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
+builder.Services.AddProblemDetails()
+    .AddExceptionHandler<GlobalExceptionHandler>();
+
 var app = builder.Build();
+
+app.UseStatusCodePages();
+app.UseExceptionHandler();
 
 app.MapGamesEndpoints();
 app.MapGenresEndpoints();
